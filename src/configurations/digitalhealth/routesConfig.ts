6a7d1ac1050a889035aa7ce98e51a70fb98c1381
jay-hodgson <jay.hodgson@sagebionetworks.org>
2020-10-29 15:12:03 -0700

    not going to work

--- src/configurations/digitalhealth/routesConfig.ts
+++ src/configurations/digitalhealth/routesConfig.ts
@@ -26,6 +26,75 @@ const routes: GenericRoute[] = [
         },
       },
       {
+        name: 'FeaturedDataTabs',
+        title: 'COLLECTIONS',
+        centerTitle: false,
+        outsideContainerClassName: 'home-spacer home-bg-dark',
+        props: {
+          sql:'SELECT * FROM syn21994974',
+          rgbIndex: 3,
+          explorePagePath:'/Explore/Collections',
+          exploreObjectType:'',
+          configs: [{
+            title: 'Studies',
+            icon: 'STUDY',
+            plotsConfig: {
+              explorePagePath: '',
+              sql: 'SELECT * FROM syn21994974 WHERE ( ( "collectionType" = \'Validation Study\' OR "collectionType" = \'Interventional Study\' OR "collectionType" = \'Observational Study\' ) )',
+              configs: [{
+                title:'The Religious Orders and Memory and Aging Project Study',
+                facetsToPlot:['dataType', 'assay'],
+                selectFacetColumnName:'study',
+                selectFacetColumnValue:'ROSMAP',
+                detailsPagePath:'/Explore/Studies/DetailsPage?Study=syn3219045'
+              },
+              {
+                title:'The Mount Sinai Brain Bank Study',
+                facetsToPlot:['dataType', 'assay'],
+                selectFacetColumnName:'study',
+                selectFacetColumnValue:'MSBB',
+                detailsPagePath:'/Explore/Studies/DetailsPage?Study=syn3159438'
+              },
+              {
+                title:'The RNAseq Harmonization Study',
+                facetsToPlot:['dataType', 'assay'],
+                selectFacetColumnName:'study',
+                selectFacetColumnValue:'rnaSeqReprocessing',
+                detailsPagePath:'/Explore/Studies/DetailsPage?Study=syn9702085'
+              }]
+            }
+          },
+          {
+            title: 'Analysis',
+            icon: 'CHART',            
+            plotsConfig: {
+              sql: 'SELECT * FROM syn21994974 WHERE ( ( "collectionType" = \'Analysis\' OR "collectionType" = \'Challenge\' ) )',
+              configs: [{                
+                title:'The UCI MODEL-AD 5XFAD Study',
+                facetsToPlot:['dataType', 'assay'],
+                selectFacetColumnName:'study',
+                selectFacetColumnValue:'UCI_5XFAD',
+                detailsPagePath:'/Explore/Studies/DetailsPage?Study=syn16798076'
+              },
+              {
+                title:'The IU/Jax/Pitt MODEL-AD Primary Screen Study',
+                facetsToPlot:['dataType', 'assay'],
+                selectFacetColumnName:'study',
+                selectFacetColumnValue:'Jax.IU.Pitt_PrimaryScreen',
+                detailsPagePath:'/Explore/Studies/DetailsPage?Study=syn21595258'
+              },
+              {
+                title:'The IU/Jax/Pit MODEL-AD APOE/TREM2 Study',
+                facetsToPlot:['dataType', 'assay'],
+                selectFacetColumnName:'study',
+                selectFacetColumnValue:'Jax.IU.Pitt_APOE4.Trem2.R47H',
+                detailsPagePath:'/Explore/Studies/DetailsPage?Study=syn17095980'
+              }
+            ]}
+          }]        
+        },
+      },
+      {
         name: 'TableFeedCards',
         title: 'What\'s New',
         centerTitle: true,
